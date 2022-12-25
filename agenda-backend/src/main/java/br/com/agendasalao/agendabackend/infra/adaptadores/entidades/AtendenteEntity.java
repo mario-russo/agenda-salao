c0b6693205c68d49bc2a5152fe58ab03c2162fba
Mário Russo <mario.russo93@hotmail.com>
2022-12-25 17:40:41 -0300

    Feat -  adição da entidade atendente

---
+++ agenda-backend/src/main/java/br/com/agendasalao/agendabackend/infra/adaptadores/entidades/AtendenteEntity.java
@@ -0,0 +1,51 @@
+package br.com.agendasalao.agendabackend.infra.adaptadores.entidades;
+
+import javax.persistence.*;
+
+import br.com.agendasalao.agendabackend.dominio.Atendente;
+
+@Entity
+public class AtendenteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public AtendenteEntity(Atendente atendente) {
+        this.nome = atendente.getNome();
+        this.email = atendente.getEmail();
+        this.senha = atendente.getSenha();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
