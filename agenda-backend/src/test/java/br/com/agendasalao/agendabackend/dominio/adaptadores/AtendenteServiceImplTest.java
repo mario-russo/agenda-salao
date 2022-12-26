04a25d80ecf883b60c2fef55999fc77e4ad71388
Mário Russo <mario.russo93@hotmail.com>
2022-12-25 22:44:56 -0300

    Teste - Inicio dos testes do caso de uso  atendente

---
+++ agenda-backend/src/test/java/br/com/agendasalao/agendabackend/dominio/adaptadores/AtendenteServiceImplTest.java
@@ -0,0 +1,24 @@
+package br.com.agendasalao.agendabackend.dominio.adaptadores;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class AtendenteServiceImplTest {
+    @Test
+    void testBuscaTodosAtendente() {
+
+    }
+
+    @Test
+    void testSalvaAtendente() {
+
+    }
+    @BeforeEach
+    public void init(){
+        
+    }
+}
