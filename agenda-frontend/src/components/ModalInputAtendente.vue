43ddf64a6df3307b8c3ab07a8f5fdbe60bd84c7e
Mário Russo <mario.russo93@hotmail.com>
2023-02-21 23:22:52 -0300

    Feat: frontend : craição do modulo atendente

---
+++ agenda-frontend/src/components/ModalInputAtendente.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import configAxios from "../axios/configAxios";
+import { Atendente } from "../domain/Atendente";
+
+
+const ph = ref("");
+const dense = ref(true);
+const dados = reactive<Atendente>({ nome: "", email:'',senha:''});
+
+function createAtendente() {
+  configAxios.post("/atendente", dados);
+}
+</script>
+
+<template>
+  <div class="filho">
+    <div>
+      <div>
+        <q-input
+          v-model="dados.nome"
+          size="lg"
+          class="input"
+          label="Nome"
+          placeholder="Insira um Nome do atendente"
+          :dense="dense"
+        >
+          <template v-slot:prepend> <i class="fa-regular fa-user"></i></template
+        ></q-input>
+      </div>
+  
+      <div>
+        <q-input
+          v-model="dados.email"
+          class="input"
+          label="email"
+          type="email"
+          placeholder="Insira um Email do atendente"
+          :dense="dense"
+        >
+          <template v-slot:prepend>
+            <i class="fa-solid fa-business-time"></i>
+          </template>
+        </q-input>
+      </div>
+      <div>
+        <q-input
+          v-model="dados.senha"
+          class="input"
+          label="senha"
+          type="password"
+          placeholder="Insira uma senha"
+          :dense="dense"
+        >
+          <template v-slot:prepend>
+            <i class="fa-solid fa-password-time"></i>
+          </template>
+        </q-input>
+      </div>
+      <q-space></q-space>
+      <div>
+        <q-btn-group spread>
+          <q-btn color="purple" label="" icon="cancel" />
+          <q-btn
+            color="purple"
+            label=""
+            icon="send"
+            @click="createAtendente()"
+          />
+        </q-btn-group>
+      </div>
+    </div>
+  </div>
+  </template>
+<style scoped>
+.filho {
+  width: 90%;
+}
+.filho div {
+  margin-bottom: 20px;
+}
+</style>
---
+++ agenda-frontend/src/domain/Atendente.ts
@@ -0,0 +1,10 @@
+export class Atendente {
+    nome: string
+    email: string
+    senha: string
+    constructor(nome: string, email: string, senha: string) {
+        this.nome = nome
+        this.senha = senha
+        this.email = email
+    }
+}
