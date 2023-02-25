21414a19b8a2f66e7e498f90e56bee5cfd0bb176
Mário Russo <mario.russo93@hotmail.com>
2023-02-25 15:32:20 -0300

    fix: recaregar no inicio da page

--- agenda-frontend/src/axios/services/AgendamentoService.ts
+++ agenda-frontend/src/axios/services/AgendamentoService.ts
@@ -2,20 +2,12 @@ import { Agendamento } from "../../domain/Agendamento"
 import configAxios from "../configAxios"
 
 export async function createAgendamento(agendamento: Agendamento) {
-    try {
         await configAxios.post('/agendamento', agendamento)
 
-    } catch (error) {
-        return error
-    }
 }
 
 export async function listAllAgendamento(): Promise<Agendamento[]> {
-    try {
         const resulado = (await configAxios.get<Agendamento[]>('/agendamento')).data
         return resulado
-    } catch (error:any) {
-        return error
-    }
-
+    
 }
